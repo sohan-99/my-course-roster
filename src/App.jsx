fa74efaccc45c4c90d259cba9495c8a88c108a7c
sohan-99 <sohan75632@gmail.com>
2023-09-14 22:48:35 +0600

    nine fake json data added

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     
+      <h1 className='text-3xl font-bold'>Course Registration</h1>
+      
+      
+    </>
+  )
+}
+
+export default App
